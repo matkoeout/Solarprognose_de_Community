d609fd8fd92893534b0b70820864a8335ae24b02
matkoerout <matthias_koerner@outlook.de>
2026-06-13 10:20:11 +0200

    Verlegen aus den Setup  und optionflows in den Code als Entity. Reset des Tageszählers durch lazy handling bei der Abfrage der Entität

--- custom_components/solarprognose_de_community/const.py
+++ custom_components/solarprognose_de_community/const.py
@@ -3,6 +3,3 @@ DOMAIN = "solarprognose_de_community"
 # Konfiguration für das Nachtfenster
 NIGHT_START_HOUR = 21
 NIGHT_END_HOUR = 3
-
-CONF_ENABLE_AUTOMATIC_POLLING = "enable_automatic_polling"
-DEFAULT_ENABLE_AUTOMATIC_POLLING = True
